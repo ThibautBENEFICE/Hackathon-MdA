6841971feb2fb98deb9694281a4f5d1c2c939292
Nicolas Maret <nicolas.maret@epita.fr>
2021-04-03 18:38:34 +0200

    [TEST][Fix] Setting up the test main

--- python/data_prep.py
+++ python/data_prep.py
@@ -37,18 +37,25 @@ class TrainFrame:
 
     def __init__(self, filename, key):
         tab_pics, info = get_pics_from_file(filename)
-        self.tab = {}
         self.tab = tab_pics
         self.passage_vect = np.zeros(info["nb_trames"])
+        self.trame_seen = 0
         self.key = from_key_to_vect(key)
         self.nb_trames = info["nb_trames"]
 
     def get_next_train_frame(self):
+        if self.trame_seen >= (0.9 * self.nb_trames):
+            self.reset_trame()
         index = np.random.randint(0, self.nb_trames - 1)
         while(self.passage_vect[index]):
             index = np.random.randint(0, self.nb_trames - 1)
         self.passage_vect[index] = 1
+        self.trame_seen += 1
         return self.tab[index]
+
+    def reset_trame(self):
+        self.passage_vect = np.zeros(self.nb_trames)
+        self.trame_seen = 0
 
 def append_tf(train_frames, key):
         filename ="../data/pics_" + key + ".bin"
@@ -77,39 +84,41 @@ def init_train_frames():
 
 
 if __name__ == "__main__":
-    epoch = 0
+    #epoch = 0
     train_frames = init_train_frames()
     mlp = MLP()
 
-    while epoch < 250000:
-        rand_key = np.random.randint(42)
-        train_frame = train_frames[rand_key]
-        trame = train_frame.get_next_train_frame()
-        expected = train_frame.key
-        mlp.train(trame, expected)
-        epoch += 1
-        print('epoch: ' + str(epoch))
-
-    #mlp.save()
-
-    epoch = 0
-    success = 0
     f = open("..\success_rate.txt", "w")
-    while epoch < 20000:
-        rand_key = np.random.randint(42)
-        train_frame = train_frames[rand_key]
-        trame = train_frame.get_next_train_frame()
-        expected = train_frame.key 
-        predicted = mlp.predict(trame)
-        #print("expected: ", expected)
-        #print("predicted: ", one_hot(predicted))
-        tmp1 = ''.join(str(elt) for elt in one_hot(predicted))
-        tmp2 = ''.join(str(elt) for elt in expected)
-        if tmp1 == tmp2:
-            success += 1
-        epoch += 1
-        #print('epoch: ' + str(epoch))
-        if epoch % 1000 == 0:
-           f.write(str(success / epoch * 100) + "\n")
-           
+    i = 0
+    while i < 10:
+        epoch = 0
+        while epoch < 20000:
+            rand_key = np.random.randint(42)
+            train_frame = train_frames[rand_key]
+            trame = train_frame.get_next_train_frame()
+            expected = train_frame.key
+            mlp.train(trame, expected)
+            epoch += 1
+            print('epoch: ' + str(epoch))
+        #mlp.save()
+        f.write("Iteration " + str(i) +'\n')  
+        epoch = 0
+        success = 0
+        somme = 0
+        while epoch < 20000:
+            rand_key = np.random.randint(42)
+            train_frame = train_frames[rand_key]
+            trame = train_frame.get_next_train_frame()
+            expected = train_frame.key 
+            predicted = mlp.predict(trame)
+            tmp1 = ''.join(str(elt) for elt in one_hot(predicted))
+            tmp2 = ''.join(str(elt) for elt in expected)
+            if tmp1 == tmp2:
+                success += 1
+            epoch += 1
+            #print('epoch: ' + str(epoch))
+            if epoch % 1000 == 0:
+               somme += success / epoch * 100
+        f.write(str(somme / 20) + '\n')
+        i += 1
     f.close()
