a362f6e858162ad5188a480bcc9a6134f51261a5
Nicolas Maret <nicolas.maret@epita.fr>
2021-04-04 16:26:15 +0200

    [TEST][Update] Cleaning previous main and result system.

--- python/data_prep.py
+++ python/data_prep.py
@@ -1,6 +1,7 @@
 import numpy as np
 from read_pics import get_pics_from_file
 from MLP import MLP, one_hot
+import progressbar
 
 def from_key_to_vect(key):
     res = np.zeros(42)
@@ -82,45 +83,87 @@ def init_train_frames():
 
     return train_frames
 
+def get_filename(maximum_learning_iteration, mlp):
+    return "../test_results/L-" + str(mlp.learning_rate).replace('.','_')\
+        + "#H-" + str(mlp.hidden_shape)\
+        + "#I-" + str(maximum_learning_iteration) + ".txt"
+
+def create_header(mlp, maximum_learning_iteration, learning_count):
+    with open('../header.txt', 'r') as h:
+        header = h.read().replace("v0", str(mlp.learning_rate))\
+                        .replace("v1", str(mlp.hidden_shape))\
+                        .replace("v2", str(maximum_learning_iteration))\
+                        .replace("v3", str(learning_count))
+    return header
+
+def train(learning_count, mlp, bar):
+    epoch = 0
+    nb_step = learning_count / 10
+    while epoch < learning_count:
+        rand_key = np.random.randint(42)
+        train_frame = train_frames[rand_key]
+        trame = train_frame.get_next_train_frame()
+        expected = train_frame.key
+        mlp.train(trame, expected)
+        if epoch % nb_step == 0:
+            bar.update(bar.currval + 1)
+        epoch += 1
+    
+def test(testing_count, testing_interval, mlp):
+    epoch = 0
+    success = 0
+    somme = 0
+    while epoch < testing_count:
+        rand_key = np.random.randint(42)
+        train_frame = train_frames[rand_key]
+        trame = train_frame.get_next_train_frame()
+        expected = train_frame.key 
+        predicted = mlp.predict(trame)
+        tmp1 = ''.join(str(elt) for elt in one_hot(predicted))
+        tmp2 = ''.join(str(elt) for elt in expected)
+        if tmp1 == tmp2:
+            success += 1
+        epoch += 1
+        if epoch % testing_interval == 0:
+            somme += success / epoch * 100
+    return (somme / (testing_count / testing_interval))
+
 
 if __name__ == "__main__":
     train_frames = init_train_frames()
-    mlp = MLP()
-    imax = 1
+    print("Initialization done. Starting learning phase, please wait...")
+    # Init neural network (input_shape, hidden_shape, output_shape, learning_rate)
+    mlp = MLP(17, 30, 42, 0.1)
+    # Nb of learning iteration to do
+    maximum_learning_iteration = 30
+    # Nb of learning by iteration
+    learning_count = 20000
+    # Nb of tests by iteration
+    testing_count = 20000
+    # testing_count / testing_interval = Number of accuracy check by iteration
+    testing_interval = 1000
 
-    filename = "../test_results/i_" + str(imax) + "_lr_" + str(mlp.learning_rate).replace('.','_') + "_hs_" + str(mlp.hidden_shape) + ".txt"
-
+    # Open the file to write result and init it with header
+    filename = get_filename(maximum_learning_iteration, mlp)
     f = open(filename, "w+")
-    i = 0
-    while i < imax:
-        epoch = 0
-        while epoch < 20000:
-            rand_key = np.random.randint(42)
-            train_frame = train_frames[rand_key]
-            trame = train_frame.get_next_train_frame()
-            expected = train_frame.key
-            mlp.train(trame, expected)
-            epoch += 1
-            print('epoch: ' + str(epoch) + " index:" + str(i))
-        #mlp.save()
-        f.write("Iteration " + str(i) +'\n')  
-        epoch = 0
-        success = 0
-        somme = 0
-        while epoch < 20000:
-            rand_key = np.random.randint(42)
-            train_frame = train_frames[rand_key]
-            trame = train_frame.get_next_train_frame()
-            expected = train_frame.key 
-            predicted = mlp.predict(trame)
-            tmp1 = ''.join(str(elt) for elt in one_hot(predicted))
-            tmp2 = ''.join(str(elt) for elt in expected)
-            if tmp1 == tmp2:
-                success += 1
-            epoch += 1
-            #print('epoch: ' + str(epoch))
-            if epoch % 1000 == 0:
-               somme += success / epoch * 100
-        f.write(str(somme / 20) + '\n')
-        i += 1
+    f.write(create_header(mlp, maximum_learning_iteration, learning_count))
+
+    # Set up progress bar
+    bar_max_val = 10 * maximum_learning_iteration
+    bar = progressbar.ProgressBar(maxval=bar_max_val, \
+        widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
+
+    learning_iteration = 0
+    while learning_iteration < maximum_learning_iteration:
+        train(learning_count, mlp, bar)
+        f.write("Learning iteration " + str(learning_iteration + 1) + "\n")  
+        result = test(testing_count, testing_interval, mlp)
+        f.write("Accuracy = " + str(result.__round__(2)) + '%\n-\n')
+        learning_iteration += 1
+    
+    bar.finish()
     f.close()
+    print("Training finished. Accuracy record saved, PATH='" + filename +"'")
+
+    #mlp.save()
